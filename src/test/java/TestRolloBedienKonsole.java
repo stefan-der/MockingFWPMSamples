0997ec3d0c775dd1247c74522baf0162e74594cc
verdaechtigerPC <stefan.der@gmail.com>
2017-10-25 15:18:32 +0200

    Refactoring & Renaming for better Readability

--- src/test/java/TestRolloBedienKonsole.java
+++ src/test/java/TestRolloBedienKonsole.java
@@ -4,41 +4,45 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestRolloBedienKonsole {
 
     @Mock
-    IRolloManagement rolloSoftwareInstanzMock = mock(IRolloManagement.class);
-    IRolloManagement rolloSoftwareInstanzSpy = spy(IRolloManagement.class);
+    IRolloManagement rolloManagementInstanzMock = mock(IRolloManagement.class);
+    @Spy
+    IRolloManagement rolloManagementInstanzSpy = spy(IRolloManagement.class);
 
-    /* Stub */
     @Test(expected = RolloSteuerungException.class)
-    public void rolloSteuerung_Reaktion_Auf_Nicht_Vorhandenen_Rollo(){
-        when(rolloSoftwareInstanzMock.hochFahrenSenden(anyInt())).thenReturn(1);
-        RolloBedienKonsole aktuelleRolloBedienKonsole =new RolloBedienKonsole(rolloSoftwareInstanzMock);
+    public void rolloBedienKonsole_Reaktion_Auf_Nicht_Vorhandenen_Rollo(){
+
+        // Record / Setup
+        when(rolloManagementInstanzMock.hochFahrenSenden(-1)).thenReturn(1);
+
+        // Replay
+        RolloBedienKonsole aktuelleRolloBedienKonsole =new RolloBedienKonsole(rolloManagementInstanzMock);
         aktuelleRolloBedienKonsole.hochFahrenKlick(-1);
+
     }
     @Test(expected = DependencyException.class)
-    public void rolloSteuerung_Reaktion_Auf_Nicht_Bekannten_Ausgabe_Der_RolloManagementSoftware(){
-        when(rolloSoftwareInstanzMock.hochFahrenSenden(anyInt())).thenReturn(100);
-        RolloBedienKonsole aktuelleRolloBedienKonsole =new RolloBedienKonsole((rolloSoftwareInstanzMock));
+    public void rolloBedienkonsole_Reaktion_Auf_Nicht_Bekannten_Ausgabe_Der_RolloManagementSoftware(){
+        when(rolloManagementInstanzMock.hochFahrenSenden(anyInt())).thenReturn(100);
+        RolloBedienKonsole aktuelleRolloBedienKonsole =new RolloBedienKonsole((rolloManagementInstanzMock));
         aktuelleRolloBedienKonsole.hochFahrenKlick(20);
     }
 
 
     /* Spy */
     @Test
-    public void rolloSteuerung_Reagiert_Normal(){
-        RolloBedienKonsole aktuelleRolloBedienKonsole =new RolloBedienKonsole(rolloSoftwareInstanzSpy);
+    public void rolloBedienKonsole_Reagiert_Normal(){
+        RolloBedienKonsole aktuelleRolloBedienKonsole =new RolloBedienKonsole(rolloManagementInstanzSpy);
         aktuelleRolloBedienKonsole.hochFahrenKlick(1);
-        Mockito.verify(rolloSoftwareInstanzSpy).hochFahrenSenden(1);
+        Mockito.verify(rolloManagementInstanzSpy).hochFahrenSenden(1);
     }
 
 
